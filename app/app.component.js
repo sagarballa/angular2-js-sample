39894f17d02fa419904d7eef601648138c03d6e9
Eswara Balla <Eswara Balla>
2016-08-13 17:05:14 +0530

    Added Part2 code

--- app/app.component.js
+++ app/app.component.js
@@ -1,17 +1,35 @@
 (function(app) {
+    function Hero(id, name) {
+        this.id = id;
+        this.name = name;
+    };
+    
+    var HEROES = [
+        { "id": 11, "name": "Mr. Nice" },
+        { "id": 12, "name": "Narco" },
+        { "id": 13, "name": "Bombasto" },
+        { "id": 14, "name": "Celeritas" },
+        { "id": 15, "name": "Magneta" },
+        { "id": 16, "name": "RubberMan" },
+        { "id": 17, "name": "Dynama" },
+        { "id": 18, "name": "Dr IQ" },
+        { "id": 19, "name": "Magma" },
+        { "id": 20, "name": "Tornado" }
+    ];
+    
     app.AppComponent =
         ng.core.Component({
-          selector: 'my-app',
-          templateUrl: 'app/app.html'
+            selector: 'my-app',
+            templateUrl: 'app/app.html',
+            styleUrls: ['app/app.css']
         })
         .Class({
           constructor: function() {
               this.title = 'Tour of Heroes  - Chapter I';
-              this.hero = {
-                id: 1,
-                name: 'Windstorm'
-            };
+              this.heros = HEROES;
           }
             
         });
+    app.AppComponent.prototype.onSelect = function (hero) { this.selectedHero = hero; };
+    
 })(window.app || (window.app = {}));
